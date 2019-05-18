8eb907d943d53fac7d82251057121fda1f09f6cc
Alexander Larin <ekzebox@gmail.com>
2019-05-19 01:58:13 +0300

    Import vector3 class from C++ library

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include "mesh.h"
 #include "skeleton.h"
+#include "vector.h"
 
 namespace py = pybind11;
 
@@ -15,6 +16,19 @@ PYBIND11_MODULE(pynocchio, m)
         .def(py::init<>());
     py::class_<FileSkeleton>(m, "FileSkeleton", skeleton)
         .def(py::init<const std::string &>());
+    py::class_<Vector3>(m, "Vector3")
+        .def(py::init<double, double, double>())
+        .def("__getitem__", [](const Vector3 &v, int i) {
+            if (i >= v.size())
+                throw py::index_error();
+            return v[i];
+        })
+        .def("__setitem__", [](Vector3 &v, int i, double value) {
+            if (i >= v.size())
+                throw py::index_error();
+            v[i] = value;
+        })
+        .def("__len__", &Vector3::size);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,10 @@ assert pynocchio.Mesh('test_mesh')
 assert pynocchio.Skeleton()
 assert pynocchio.HumanSkeleton()
 assert pynocchio.FileSkeleton('test_skeleton')
+v = pynocchio.Vector3(0., 1., 2.)
+assert len(v) == 3
+assert v[0] == 0.
+assert v[1] == 1.
+assert v[2] == 2.
+v[1] = 5.
+assert v[1] == 5.
