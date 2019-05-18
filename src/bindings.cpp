3a165932bea744a509259b295c1f95ef936afee9
Alexander Larin <ekzebox@gmail.com>
2019-05-19 01:46:35 +0300

    Import skeleton basic classes from C++ library

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,11 +1,19 @@
 #include <pybind11/pybind11.h>
 #include "mesh.h"
+#include "skeleton.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(pynocchio, m)
 {
     py::class_<Mesh>(m, "Mesh")
+        .def(py::init<const std::string &>());
+    py::class_<Skeleton> skeleton(m, "Skeleton");
+    skeleton
+        .def(py::init<>());
+    py::class_<HumanSkeleton>(m, "HumanSkeleton", skeleton)
+        .def(py::init<>());
+    py::class_<FileSkeleton>(m, "FileSkeleton", skeleton)
         .def(py::init<const std::string &>());
 
 #ifdef VERSION_INFO
